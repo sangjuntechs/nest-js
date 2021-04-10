e10e10b4149ccc18a1eda2e26178b21f0cc409a7
sangjuntechs <juntech0421@github.com>
2021-04-10 16:44:16 +0900

    decorator create

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,14 +7,19 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
+  @Get('search')
+  search(@Query('year') searchYear: string) {
+    return `${searchYear}년도 이후의 영화들을 검색합니다.`;
+  }
+
   @Get('/:id')
   getOne(@Param('id') movieId: string) {
     return `This will return one movie with the id: ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() movieData) {
+    return `movie name is ${movieData.name} director is ${movieData.director}`;
   }
 
   @Delete()
@@ -22,8 +27,11 @@ export class MoviesController {
     return `This will delete a movie with the id: ${movieId}`;
   }
 
-  @Patch()
-  update(@Param('id') movieId: string) {
-    return `This will update a movie with the id: ${movieId}`;
+  @Patch('/:id')
+  update(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updateMovie: movieId,
+      ...updateData,
+    };
   }
 }
